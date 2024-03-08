249ef390e6ebb219ee9cbc12b6d32fd34a253c86
rodrigogrego <rodrigogrego18@gmail.com>
2024-03-08 15:39:21 -0300

    :sparkles: feat: Gestão de roteamento inicial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+
+
+<div style="width:100%; height:100%; display:flex; align-items:center;  justify-content:center;">
+    <div style="display:flex; flex-direction:column; align-items:center">
+        <h1>GESTÃO DE HORAS</h1>
+        <form action="">
+            <div style="display:flex; flex-direction:column">
+
+                <label for="">Login</label>
+                <input type="text" id="login">
+                <label for="pasword">Senha</label>
+                <input type="password" name="" id="password">
+            </div>
+        </form>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\EnrollmentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [ HomeController::class, 'index' ])->name('home');
+
+Route::prefix('app')->group(function(){
+
+    Route::get('/firstpage', [ EnrollmentController::class, 'index'])->name('app.firstpage');
+
+    Route::get('/dashboard', [ EnrollmentController::class, 'dashboard'])->name('app.dashboard');
+
 });
+
+Route::fallback(function() {
+    echo 'Pagina não disponível. <a href="'.route('home').'">Acessa aqui a página principal.</a>';
+});
+
