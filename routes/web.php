4f2d5190b7d8d6c071bfe473bf4af218a1e0930a
rodrigogrego <rodrigogrego18@gmail.com>
2024-04-08 11:52:54 -0300

    Utilização do laravel/ui primeiro commit

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\EnrollmentController;
-use App\Http\Controllers\ContatoController;
-use App\Http\Middleware\LogAcessosMiddleware;
-use App\Http\Middleware\AutenticacaoMiddleware;
-use App\Http\Controllers\HomeController;
 
 
 
@@ -21,26 +15,6 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/{error?}', [ LoginController::class, 'index' ])->name('login');
+Auth::routes();
 
-Route::post('/', [ LoginController::class, 'authenticate' ])->name('auth');
-
-Route::get('/contato', [ ContatoController::class, 'index'])->name('contato');
-Route::post('/contato', [ ContatoController::class, 'store'])->name('contato');
-
-Route::middleware('autenticacao')->prefix('app')->group(function(){
-
-    Route::get('/home', [ HomeController::class, 'index' ])->name('app.home');
-
-    Route::get('/logout', [ LoginController::class, 'logout'])->name('app.logout');
-
-    Route::get('/firstpage', [ EnrollmentController::class, 'index'])->name('app.firstpage');
-
-    Route::get('/dashboard', [ EnrollmentController::class, 'dashboard'])->name('app.dashboard');
-
-});
-
-Route::fallback(function(){
-    echo 'Pagina não disponível. <a href="'.route('login').'">Acessa aqui a página principal.</a>';
-});
-
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
