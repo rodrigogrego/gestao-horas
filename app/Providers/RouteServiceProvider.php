fe52ea60d8fd69455b87c2f50c790727ff815736
rodrigogrego <rodrigogrego18@gmail.com>
2024-04-17 15:26:45 -0300

    Provide udpate Route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/tarefa';
+    public const HOME = '/tarefas';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,15 @@ use App\Mail\MensagemTesteMail;
 |
 */
 Route::get('/', function(){
-    return view('welcome');
+    return view('auth.login');
 });
 
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
-                    ->name('home')
-                    ->middleware('verified');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+//                     ->name('home')
+//                     ->middleware('verified');
 
 Route::resource('tarefas', 'App\Http\Controllers\TarefaController')
 ->middleware('verified');
